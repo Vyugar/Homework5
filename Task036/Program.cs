fcd43e624f11df147ebfcc4bc7277388ab12b038
Vyugar <vyugar@yandex.ru>
2022-11-27 17:05:01 +0300

    aded Task036

---
+++ Task036/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+*/
+
+int[] GetRandomArray(int length, int min, int max)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int GetSumOfOddIndex(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum = sum + array[i];
+        }
+    }
+    return sum;
+}
+
+int[] newArray = GetRandomArray(8, -9, 99);
+Console.ForegroundColor = ConsoleColor.Magenta;
+PrintArray(newArray);
+Console.ResetColor();
+int summa = GetSumOfOddIndex(newArray);
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях = {summa}. ");
+Console.ResetColor();
