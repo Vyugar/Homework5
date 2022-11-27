0ea2bbbc33854941cb63c3f3ead32aad80c88198
Vyugar <vyugar@yandex.ru>
2022-11-27 16:31:53 +0300

    aded Task034

---
+++ Task034/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+*/
+
+int[] GetRandomArray(int length, int min, int max)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+int GetEvenNumberArray(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int[] newArray = GetRandomArray(8, 100, 999);
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+PrintArray(newArray);
+Console.ResetColor();
+int count = GetEvenNumberArray(newArray);
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine($"Количество четных чисел в массиве = {count}. ");
+Console.ResetColor();
